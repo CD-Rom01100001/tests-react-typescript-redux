25d5d0ddce9813cd3cb5bda33bb75580e17f2999
unknown <romanchernyshkov88@gmail.com>
2024-12-24 21:53:14 +0300

    refactor: created the basic structure of the project files

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import css from './app.module.css'
+
+const App: FC = () => {
+
+  return (
+    <div className={css.app}>
+      
+    </div>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import './styles/fonsts.css'
 import './styles/normalize.css'
-import App from './App.tsx'
+import App from './components/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
