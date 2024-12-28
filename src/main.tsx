ee8b10879fe49720d15886085d867f0c1cb98fda
unknown <romanchernyshkov88@gmail.com>
2024-12-28 12:25:15 +0300

    refactor: added redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { store } from './store/store.ts'
+import { Provider } from 'react-redux'
 import './styles/index.css'
 import './styles/fonsts.css'
 import './styles/normalize.css'
@@ -7,6 +9,8 @@ import App from './components/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 )
