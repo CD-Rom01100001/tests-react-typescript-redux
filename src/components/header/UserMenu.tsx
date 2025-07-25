64670aeaf416b5b1bfb2c3ebc4eb849800d6cc31
Roman <69603786+CD-Rom01100001@users.noreply.github.com>
2025-07-25 18:16:19 +0300

    repositioned the “exit” button

--- src/components/header/UserMenu.tsx
+++ src/components/header/UserMenu.tsx
@@ -1,10 +1,23 @@
 import { FC } from 'react';
-import { useAppSelector } from '../../store/hooks';
+import { Link } from 'react-router-dom';
+
+/* redux */
+import { useAppSelector, useAppDispatch } from '../../store/hooks';
+import { getUserDate } from '../../store/userSlice';
+import { resetResultsData } from '../../store/setResultsSlice';
+import { setStateUserWindow } from '../../store/userWindowSlice';
+
+/* css */
 import css from './UserMenu.module.css'
 
 const UserMenu: FC = () => {
+  const dispatch = useAppDispatch()
+  const user = useAppSelector(state => state.userDataIndex.user)
   const userDate = useAppSelector(state => state.userDataIndex.user)
   const userWindowState = useAppSelector(state => state.userWindow.window)
+  const theme = useAppSelector(state => state.themeIndex.themeSlice).toLocaleLowerCase();
+  const locationAdmin = location.pathname.startsWith('/settings');// проверка на соответствие шаблона адреса
+  const locationModerator = location.pathname.startsWith('/users');// проверка на соответствие шаблона адреса
 
   const convertRoleToText = () => {
     switch (userDate?.role) {
@@ -19,18 +32,49 @@ const UserMenu: FC = () => {
     }
   }
 
-
+  const logout = () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    localStorage.removeItem('resultsTrainingData')
+    localStorage.removeItem('resultsExamData')
+    localStorage.removeItem('userList')
+    dispatch(getUserDate(null))
+    dispatch(resetResultsData())
+    dispatch(setStateUserWindow())
+  }
 
   return (
     <div className={`${css.userMenu} ${userWindowState ? css.active : ''}`}>
-      <h3>ФИО</h3>
-      <p>{userDate?.lastName}</p>
-      <p>{userDate?.firstName}</p>
-      <p>{userDate?.middleName}</p>
+      <div className={css.fioBlock}>
+        <h3>ФИО</h3>
+        <p>{userDate?.lastName}</p>
+        <p>{userDate?.firstName}</p>
+        <p>{userDate?.middleName}</p>
+      </div>
 
-      <h3 style={{marginTop: '2rem'}}>Информация</h3>
-      <p>email: {userDate?.email}</p>
-      <p>статус: {convertRoleToText()}</p>
+      <div className={css.infoBlock}>
+        <h3>Информация</h3>
+        <p>email: {userDate?.email}</p>
+        <p>статус: {convertRoleToText()}</p>
+      </div>
+
+      <div className={css.blockButton}>
+        {
+            /* если пользователь авторизован и путь localhost:5173/settings или localhost:5173/users то кнопка "Выход" и ведет она себя как ссылка и ведет на главную страницу */
+          user && locationAdmin || locationModerator ? 
+          (
+            <Link to="/" className={`${css.logIn} buttonDef ${theme}`} onClick={logout}>
+              Выход
+            </Link>
+          ) : 
+          /* если путь не localhost:5173/admin или не localhost:5173/users и пользователь авторизован то кнопка "Выход" и ведет она себя как обычно */
+          (
+            <button className={`${css.logIn} buttonDef ${theme}`} onClick={logout}>
+              Выход
+            </button>
+          )
+        }
+      </div>
     </div>
   );
 }
